69273599763c8b78ddf004659e0f8b1969019f88
lihaozhen <bp34@163.com>
2014-12-17 16:14:22 +0800

    1.处理协议回调 2.添加注释

--- BPKITsDemo/BPKITsDemo/RootViewController.m
+++ BPKITsDemo/BPKITsDemo/RootViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "RootViewController.h"
+#import "BPChanged.h"
+#import "BPChangedViewController.h"
 
 @interface RootViewController ()
 
@@ -20,9 +22,33 @@
     
     if ([sender isKindOfClass:[UITableViewCell class]]) {
         
+        //获取cell上的文字
         UITableViewCell *cell = sender;
         NSString *title = cell.textLabel.text;
-        [(UIViewController *)segue.destinationViewController setTitle:title];
+        
+        //将文字设置为下一页面的标题
+        UIViewController *destinationViewController = segue.destinationViewController;
+        [destinationViewController setTitle:title];
+        
+        //如果目标页面已实现 <BPCahnged> 协议
+        if ([destinationViewController conformsToProtocol:@protocol(BPChanged)]) {
+            
+            //如果目标页面是 BPChangedViewController
+            if ([destinationViewController isKindOfClass:[BPChangedViewController class]]) {
+                
+                //强转
+                BPChangedViewController *chagnedViewController = (BPChangedViewController *)destinationViewController;
+                
+                //设置监听回调
+                [chagnedViewController setChangedBlock:^(NSNumber *obj, BOOL isChanged){
+                    
+                    if (isChanged == YES) {
+                        //如果发生变化，将新的数值显示在cell的detailTextLabel上
+                        cell.detailTextLabel.text = [obj stringValue];
+                    }
+                }];
+            }
+        }
     }
     
     
