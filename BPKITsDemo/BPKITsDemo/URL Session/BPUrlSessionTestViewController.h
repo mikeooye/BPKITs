6e9848396f4b7803aa71b25220665e9d51009ab7
mikeooye <mikeooye@TSJ-LHZ.local>
2015-04-01 18:19:02 +0800

    添加NSURLSessionTask

---
+++ BPKITsDemo/BPKITsDemo/URL Session/BPUrlSessionTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  BPUrlSessionTestViewController.h
+//  BPKITsDemo
+//
+//  Created by mikeooye on 15-3-30.
+//  Copyright (c) 2015年 ihojin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPUrlSessionTestViewController : UIViewController
+
+@end
---
+++ BPKITsDemo/BPKITsDemo/URL Session/BPUrlSessionTestViewController.m
@@ -0,0 +1,43 @@
+//
+//  BPUrlSessionTestViewController.m
+//  BPKITsDemo
+//
+//  Created by mikeooye on 15-3-30.
+//  Copyright (c) 2015年 ihojin. All rights reserved.
+//
+
+#import "BPUrlSessionTestViewController.h"
+
+
+@interface BPUrlSessionTestViewController ()<NSURLSessionDelegate, NSURLSessionTaskDelegate>
+
+@end
+
+@implementation BPUrlSessionTestViewController{
+    
+    NSURLSession *_session;
+}
+
+
+- (void)viewDidLoad
+{
+    _session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                             delegate:self
+                                        delegateQueue:[NSOperationQueue mainQueue]];
+}
+
+- (IBAction)taskResume:(id)sender
+{
+    
+    NSString *url = @"http://m2.qiushibaike.com/article/list/text?count=30&page=1";
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    NSURLSessionTask *task = [_session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        if (error == nil) {
+            id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+        }
+        
+    }];
+    [task resume];
+}
+@end
