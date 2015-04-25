56e5e64ab78094968f06750107e582eac1b66b54
LiHaozhen <sMike@macdeiMac.local>
2015-04-25 18:07:59 +0800

    修复 文字不能全部显示的问题

--- Views/BPBarItemButton.m
+++ Views/BPBarItemButton.m
@@ -12,8 +12,9 @@
 
 - (CGRect)imageRectForContentRect:(CGRect)contentRect
 {
+    
     CGRect frame = [super imageRectForContentRect:contentRect];
-    CGRect titleFrame = [self titleRectForContentRect:contentRect];
+    CGRect titleFrame = [super titleRectForContentRect:contentRect];
     
     frame = CGRectOffset(frame, CGRectGetWidth(titleFrame) * 0.5, -CGRectGetHeight(titleFrame) * 0.5);
     return frame;
@@ -24,6 +25,14 @@
     CGRect frame = [super titleRectForContentRect:contentRect];
     CGRect imageRect = [super imageRectForContentRect:contentRect];
     frame = CGRectOffset(frame, -CGRectGetWidth(imageRect) * 0.5, CGRectGetHeight(imageRect) * 0.5);
+    frame.origin.x = contentRect.origin.x;
+    frame.size.width = contentRect.size.width;
     return frame;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    self.titleLabel.textAlignment = NSTextAlignmentCenter;
 }
 @end
