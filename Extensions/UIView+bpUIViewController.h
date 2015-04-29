9799c1634c265c523e6c4177a107481e59dfa3e4
sMike <bp34@163.com>
2015-04-29 11:28:25 +0800

    添加DismissButton

---
+++ Extensions/UIView+bpUIViewController.h
@@ -0,0 +1,14 @@
+//
+//  UIView+bpUIViewController.h
+//  StoryboardDemo
+//
+//  Created by LiHaozhen on 15/4/29.
+//  Copyright (c) 2015年 LiHaozhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (bpUIViewController)
+
+@property (readonly, nonatomic) UIViewController *viewController;
+@end
---
+++ Views/BPDismissButton.h
@@ -0,0 +1,13 @@
+//
+//  BPDismissButton.h
+//  StoryboardDemo
+//
+//  Created by LiHaozhen on 15/4/29.
+//  Copyright (c) 2015年 LiHaozhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPDismissButton : UIButton
+
+@end
