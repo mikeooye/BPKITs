ab6b53aebb436af2a714af7ea81c5cd949018e19
sMike <bp34@163.com>
2015-05-04 13:36:52 +0800

    使用BPLocationManager 代替 CLLocationManager

--- BPKITsDemo/BPKITsDemo/Util/Location/BPLocationDemoViewController.m
+++ BPKITsDemo/BPKITsDemo/Util/Location/BPLocationDemoViewController.m
@@ -10,10 +10,11 @@
 #import <CoreLocation/CoreLocation.h>
 #import "CLPlacemark+bpExtension.h"
 #import "RFLocationConvertor.h"
+#import "BPLocationManager.h"
 
-@interface BPLocationDemoViewController ()<CLLocationManagerDelegate>
+@interface BPLocationDemoViewController ()
 
-@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) BPLocationManager *locationManager;
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (strong, nonatomic) CLGeocoder *geocoder;
 @end
@@ -22,96 +23,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     _geocoder = [[CLGeocoder alloc] init];
     
-    _locationManager = [[CLLocationManager alloc] init];
-    _locationManager.distanceFilter = 20;
-    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    _locationManager.delegate = self;
+    self.locationManager = [BPLocationManager sharedManager];
     
-    if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
-        [self.locationManager requestWhenInUseAuthorization];
-    }
-    
-    [_locationManager startUpdatingLocation];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - LocationManager delegate
-
-- (void)locationManager:(CLLocationManager *)manager
-     didUpdateLocations:(NSArray *)locations
-{
-    NSLog(@"locations: %@", locations);
-    
-    CLLocation *newLocation = [locations firstObject];
-    if (newLocation) {
+    __weak typeof(self) wkSelf = self;
+    [self.locationManager startUpdatingLocationWithCallback:^(CLLocation *newLocation, NSError *error) {
         
-        NSTimeInterval age = [newLocation.timestamp timeIntervalSinceNow];
-        if (age < 5.0f) {
-            NSString *fmt = @"GPS Location: %.8f,%.8f\n";
-            [self.textView setText:[NSString stringWithFormat:
-                                    fmt,
-                                    newLocation.coordinate.latitude,
-                                    newLocation.coordinate.longitude]];
+        if (newLocation) {
             
-            __weak typeof(self) wkSelf = self;
-            
-            [RFLocationConvertor AMapCoordinateFromLocation:newLocation
-                                           coordinateSystem:AMapCoordinateSystem_GPS
-                                             successHandler:
-             ^(CLLocation *destinationLocation) {
-                 
-                 NSString *fmt = @"高德 Location: %.8f,%.8f\n";
-                 
-                 NSString *preText = wkSelf.textView.text;
-                 NSString *text = [preText stringByAppendingString:[NSString stringWithFormat:
-                                                                    fmt,
-                                                                    destinationLocation.coordinate.latitude,
-                                                                    destinationLocation.coordinate.longitude]];
-                 [wkSelf.textView setText:text];
-                 
-                 
-                 if (wkSelf.geocoder.isGeocoding) {
-                     [wkSelf.geocoder cancelGeocode];
-                 }
-                 
-                 [wkSelf.geocoder reverseGeocodeLocation:destinationLocation completionHandler:
-                  ^(NSArray *placemarks, NSError *error) {
-                      
-                      if (error == nil) {
-                          CLPlacemark *placemark = [placemarks firstObject];
-                          if (placemark && placemark.addressDictionary) {
-                              
-                              NSString *preText = wkSelf.textView.text;
-                              NSString *text = [preText stringByAppendingString:[placemark formattedAddressWithCountryName:NO]];
-                              [wkSelf.textView setText:text];
+            NSTimeInterval age = [newLocation.timestamp timeIntervalSinceNow];
+            if (age < 5.0f) {
+                NSString *fmt = @"GPS Location: %.8f,%.8f\n";
+                [wkSelf.textView setText:[NSString stringWithFormat:
+                                          fmt,
+                                          newLocation.coordinate.latitude,
+                                          newLocation.coordinate.longitude]];
+                
+                [RFLocationConvertor AMapCoordinateFromLocation:newLocation
+                                               coordinateSystem:AMapCoordinateSystem_GPS
+                                                 successHandler:
+                 ^(CLLocation *destinationLocation) {
+                     
+                     NSString *fmt = @"高德 Location: %.8f,%.8f\n";
+                     
+                     NSString *preText = wkSelf.textView.text;
+                     NSString *text = [preText stringByAppendingString:[NSString stringWithFormat:
+                                                                        fmt,
+                                                                        destinationLocation.coordinate.latitude,
+                                                                        destinationLocation.coordinate.longitude]];
+                     [wkSelf.textView setText:text];
+                     
+                     
+                     if (wkSelf.geocoder.isGeocoding) {
+                         [wkSelf.geocoder cancelGeocode];
+                     }
+                     
+                     [wkSelf.geocoder reverseGeocodeLocation:destinationLocation completionHandler:
+                      ^(NSArray *placemarks, NSError *error) {
+                          
+                          if (error == nil) {
+                              CLPlacemark *placemark = [placemarks firstObject];
+                              if (placemark && placemark.addressDictionary) {
+                                  
+                                  NSString *preText = wkSelf.textView.text;
+                                  NSString *text = [preText stringByAppendingString:[placemark formattedAddressWithCountryName:NO]];
+                                  [wkSelf.textView setText:text];
+                              }
                           }
-                      }
-                  }];
-             }];
+                      }];
+                 }];
+            }
         }
-    }
+    }];
 }
 
-- (void)locationManager:(CLLocationManager *)manager
-       didFailWithError:(NSError *)error
+- (void)dealloc
 {
-    if (error.code != kCLErrorLocationUnknown) {
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"错误"
-                                                                       message:error.localizedDescription
-                                                                preferredStyle:UIAlertControllerStyleAlert];
-        [alert addAction:[UIAlertAction actionWithTitle:@"确定"
-                                                  style:UIAlertActionStyleDefault handler:nil]];
-        [self presentViewController:alert
-                           animated:YES completion:nil];
-    }
+    [self.locationManager stopUpdatingLocation];
 }
-
 @end
