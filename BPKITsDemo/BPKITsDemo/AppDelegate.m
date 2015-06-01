51a656b4b3d011b88a260a6e7d9a459ddb89a13f
LiHaozhen <bp34@163.com>
2015-06-01 16:58:45 +0800

    将通知注册移动到Appdelegate中

--- BPKITsDemo/BPKITsDemo/AppDelegate.m
+++ BPKITsDemo/BPKITsDemo/AppDelegate.m
@@ -20,8 +20,78 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    [self registerNotification];
+    
     [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerFired) userInfo:nil repeats:YES];
+    UILocalNotification *localNote = [launchOptions objectForKeyedSubscript:UIApplicationLaunchOptionsLocalNotificationKey];
+    if (localNote) {
+        [self handleLocalNotification:localNote];
+    }
     return YES;
+}
+
+- (void)registerNotification
+{
+    UIUserNotificationType userNotType = (UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound);
+    
+    UIMutableUserNotificationAction *acceptAction = [[UIMutableUserNotificationAction alloc] init];
+    acceptAction.identifier = @"accept.identifier";
+    acceptAction.title = @"Accept";
+    acceptAction.activationMode = UIUserNotificationActivationModeBackground;
+    //如果Yes，将需要密码验证，但不解除锁定
+    acceptAction.authenticationRequired = NO;
+    acceptAction.destructive = NO;
+    
+    UIMutableUserNotificationAction *trashAction = [[UIMutableUserNotificationAction alloc] init];
+    trashAction.identifier = @"trash.identifier";
+    trashAction.title = @"Trash";
+    trashAction.activationMode = UIUserNotificationActivationModeBackground;
+    trashAction.authenticationRequired = YES;
+    trashAction.destructive = YES;
+    
+    UIMutableUserNotificationAction *replyAction = [[UIMutableUserNotificationAction alloc] init];
+    replyAction.identifier = @"reply.identifier";
+    replyAction.title = @"Reply";
+    replyAction.activationMode = UIUserNotificationActivationModeForeground;
+    replyAction.authenticationRequired = YES;
+    replyAction.destructive = NO;
+    
+    UIMutableUserNotificationCategory *inviteCategory = [[UIMutableUserNotificationCategory alloc] init];
+    inviteCategory.identifier = @"invite.identifier";
+    [inviteCategory setActions:@[acceptAction, trashAction, replyAction] forContext:UIUserNotificationActionContextDefault];
+    
+    
+    UIMutableUserNotificationAction *likeAct = [[UIMutableUserNotificationAction alloc] init];
+    likeAct.identifier = @"like.identifier";
+    likeAct.title = @"喜欢";
+    likeAct.activationMode = UIUserNotificationActivationModeBackground;
+    likeAct.authenticationRequired = NO;
+    likeAct.destructive = NO;
+    
+    UIMutableUserNotificationAction *commentAct = [[UIMutableUserNotificationAction alloc] init];
+    commentAct.identifier = @"comment.identifier";
+    commentAct.title = @"评论";
+    commentAct.activationMode = UIUserNotificationActivationModeForeground;
+    commentAct.authenticationRequired = NO;
+    commentAct.destructive = NO;
+    
+    UIMutableUserNotificationAction *tagAct = [[UIMutableUserNotificationAction alloc] init];
+    tagAct.identifier = @"tag.identifier";
+    tagAct.title = @"Tag";
+    tagAct.activationMode = UIUserNotificationActivationModeBackground;
+    tagAct.authenticationRequired = NO;
+    tagAct.destructive = YES;
+    
+    UIMutableUserNotificationCategory *tagCategory = [[UIMutableUserNotificationCategory alloc] init];
+    tagCategory.identifier = @"tag.identifier";
+    [tagCategory setActions:@[likeAct, commentAct, tagAct] forContext:UIUserNotificationActionContextMinimal];
+    
+    NSSet *categories = [NSSet setWithObjects:inviteCategory, tagCategory, nil];
+    
+    UIUserNotificationSettings *userNotSettings = [UIUserNotificationSettings settingsForTypes:userNotType
+                                                                                    categories:categories];
+    
+    [[UIApplication sharedApplication] registerUserNotificationSettings:userNotSettings];
 }
 
 - (void)timerFired
@@ -34,26 +104,21 @@
         NSLog(@"%@", [[UIApplication sharedApplication] currentNetworkStatusDescription]);
     }
 }
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    [self handleLocalNotification:notification];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)handleLocalNotification:(UILocalNotification *)notification
+{
+    
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (void)application:(UIApplication *)application handleActionWithIdentifier:(NSString *)identifier forLocalNotification:(UILocalNotification *)notification completionHandler:(void (^)())completionHandler
+{
+    if (completionHandler) {
+        completionHandler();
+    }
 }
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 @end
--- BPKITsDemo/BPKITsDemo/Notification/NotificationRootViewController.m
+++ BPKITsDemo/BPKITsDemo/Notification/NotificationRootViewController.m
@@ -25,66 +25,23 @@
  */
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
 
-    UIUserNotificationType userNotType = (UIUserNotificationTypeAlert|UIUserNotificationTypeBadge|UIUserNotificationTypeSound);
-    
-    UIMutableUserNotificationAction *acceptAction = [[UIMutableUserNotificationAction alloc] init];
-    acceptAction.identifier = @"accept.identifier";
-    acceptAction.title = @"Accept";
-    acceptAction.activationMode = UIUserNotificationActivationModeBackground;
-    //如果Yes，将需要密码验证，但不解除锁定
-    acceptAction.authenticationRequired = NO;
-    acceptAction.destructive = NO;
-    
-    UIMutableUserNotificationAction *trashAction = [[UIMutableUserNotificationAction alloc] init];
-    trashAction.identifier = @"trash.identifier";
-    trashAction.title = @"Trash";
-    trashAction.activationMode = UIUserNotificationActivationModeBackground;
-    trashAction.authenticationRequired = YES;
-    trashAction.destructive = YES;
-    
-    UIMutableUserNotificationAction *replyAction = [[UIMutableUserNotificationAction alloc] init];
-    replyAction.identifier = @"reply.identifier";
-    replyAction.title = @"Reply";
-    replyAction.activationMode = UIUserNotificationActivationModeForeground;
-    replyAction.authenticationRequired = YES;
-    replyAction.destructive = NO;
-    
-    UIMutableUserNotificationCategory *inviteCategory = [[UIMutableUserNotificationCategory alloc] init];
-    inviteCategory.identifier = @"invite.identifier";
-    [inviteCategory setActions:@[acceptAction, trashAction, replyAction] forContext:UIUserNotificationActionContextDefault];
-    
-    
-    UIMutableUserNotificationAction *likeAct = [[UIMutableUserNotificationAction alloc] init];
-    likeAct.identifier = @"like.identifier";
-    likeAct.title = @"Like";
-    likeAct.activationMode = UIUserNotificationActivationModeBackground;
-    likeAct.authenticationRequired = NO;
-    likeAct.destructive = NO;
-    
-    UIMutableUserNotificationAction *commentAct = [[UIMutableUserNotificationAction alloc] init];
-    commentAct.identifier = @"comment.identifier";
-    commentAct.title = @"Comment";
-    commentAct.activationMode = UIUserNotificationActivationModeBackground;
-    commentAct.authenticationRequired = NO;
-    commentAct.destructive = NO;
-    
-    UIMutableUserNotificationAction *tagAct = [[UIMutableUserNotificationAction alloc] init];
-    tagAct.identifier = @"tag.identifier";
-    tagAct.title = @"Tag";
-    tagAct.activationMode = UIUserNotificationActivationModeBackground;
-    tagAct.authenticationRequired = NO;
-    tagAct.destructive = YES;
-    
-    UIMutableUserNotificationCategory *tagCategory = [[UIMutableUserNotificationCategory alloc] init];
-    tagCategory.identifier = @"tag.identifier";
-    [tagCategory setActions:@[likeAct, commentAct, tagAct] forContext:UIUserNotificationActionContextMinimal];
-    
-    
-    UIUserNotificationSettings *userNotSettings = [UIUserNotificationSettings settingsForTypes:userNotType
-                                                                                    categories:[NSSet setWithObjects:inviteCategory, tagCategory, nil]];
-    
-    [[UIApplication sharedApplication] registerUserNotificationSettings:userNotSettings];
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self scheduleTestLocalNotification];
+}
+
+- (void)scheduleTestLocalNotification
+{
+    UILocalNotification *localNote = [[UILocalNotification alloc] init];
+    localNote.category = @"tag.identifier";
+    localNote.alertTitle = @"Test title";
+    localNote.alertBody = @"test message";
+    localNote.alertAction = @"Action";
+    localNote.fireDate = [NSDate dateWithTimeIntervalSinceNow:5];
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNote];
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
